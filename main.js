d1f92ea19520fa36aede362520d03e33b5b46e3b
Pawel Skrobski <skrobskipawel@gmail.com>
2022-05-08 17:52:16 +0200

    Switched to Swiper JS library and made slider usable

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
+// const CARDS_CONTAINER = [...document.querySelectorAll('.cards')];
+// const CARD = document.getElementById('nb2002rc-card');
+
+// const LEFT_BUTTON = document.getElementById('cards-button-left');
+// const RIGHT_BUTTON = document.getElementById('cards-button-right');
+
+// const COVER = document.getElementById('cover');
+
+// const IMG_PATH = '/img/';
+
+// const BACKGROUND_ARR = [`${IMG_PATH}parra_full.jpeg`, `${IMG_PATH}2002rc.jpeg`, `${IMG_PATH}travis_dunk_full.jpeg`]
+
+// let counter = 0;
 
 
-console.log('elo');
+const timer = 2500;
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    autoplay: {
+        delay: timer,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+
+    },
+    navigation: {
+        nextEl: ".swiper-button-right",
+        prevEl: ".swiper-button-left",
+    },
+
+});
+
+swiper.on('slideChange', function () {
+    document.getElementById('slider-number').textContent = `0${this.activeIndex + 1}`;
+});
+
+
+
+
+
+
+CARDS_CONTAINER.forEach((item, i) => {
+
+    RIGHT_BUTTON.addEventListener('click', () => {
+
+        COVER.style.backgroundImage = `url(${BACKGROUND_ARR[counter]})`;
+        if (counter < BACKGROUND_ARR.length - 1) {
+            return counter++;
+        }
+
+        return counter = 0;
+    });
+
+    LEFT_BUTTON.addEventListener('click', () => {
+        COVER.style.backgroundImage = `url(${BACKGROUND_ARR[counter]})`;
+        if (counter > 0) {
+            return counter--;
+        }
+
+        return counter = BACKGROUND_ARR.length - 1;
+    });
+})
+
+
+
