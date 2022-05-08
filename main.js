724acc2d72dfe9e1fc8fc71218600269955cc803
Pawel Skrobski <skrobskipawel@gmail.com>
2022-05-08 19:52:18 +0200

    Added and replaced background pictures

--- main.js
+++ main.js
@@ -4,11 +4,20 @@
 // const LEFT_BUTTON = document.getElementById('cards-button-left');
 // const RIGHT_BUTTON = document.getElementById('cards-button-right');
 
-// const COVER = document.getElementById('cover');
+const COVER = document.getElementById('cover');
 
-// const IMG_PATH = '/img/';
+const IMG_PATH = '/img/';
 
-// const BACKGROUND_ARR = [`${IMG_PATH}parra_full.jpeg`, `${IMG_PATH}2002rc.jpeg`, `${IMG_PATH}travis_dunk_full.jpeg`]
+const BACKGROUND_ARR =
+    [
+        `${IMG_PATH}parra_full.jpeg`,
+        `${IMG_PATH}2002rc_full.jpg`,
+        `${IMG_PATH}travis_dunk_full.webp`,
+        `${IMG_PATH}aime_550_full.jpg`,
+        `${IMG_PATH}travis_dunk_full.webp`,
+        `${IMG_PATH}rich_550_full.png`,
+        `${IMG_PATH}travis_dunk_full.webp`,
+    ]
 
 // let counter = 0;
 
@@ -38,34 +47,37 @@ var swiper = new Swiper(".mySwiper", {
 
 swiper.on('slideChange', function () {
     document.getElementById('slider-number').textContent = `0${this.activeIndex + 1}`;
+    backgroundChange(COVER, BACKGROUND_ARR, this.activeIndex)
 });
 
 
+function backgroundChange(element, arr, i) {
+    return element.style.backgroundImage = `url(${arr[i]})`;
+}
 
 
 
+// CARDS_CONTAINER.forEach((item, i) => {
 
-CARDS_CONTAINER.forEach((item, i) => {
+//     RIGHT_BUTTON.addEventListener('click', () => {
 
-    RIGHT_BUTTON.addEventListener('click', () => {
+//         COVER.style.backgroundImage = `url(${BACKGROUND_ARR[counter]})`;
+//         if (counter < BACKGROUND_ARR.length - 1) {
+//             return counter++;
+//         }
 
-        COVER.style.backgroundImage = `url(${BACKGROUND_ARR[counter]})`;
-        if (counter < BACKGROUND_ARR.length - 1) {
-            return counter++;
-        }
+//         return counter = 0;
+//     });
 
-        return counter = 0;
-    });
+//     LEFT_BUTTON.addEventListener('click', () => {
+//         COVER.style.backgroundImage = `url(${BACKGROUND_ARR[counter]})`;
+//         if (counter > 0) {
+//             return counter--;
+//         }
 
-    LEFT_BUTTON.addEventListener('click', () => {
-        COVER.style.backgroundImage = `url(${BACKGROUND_ARR[counter]})`;
-        if (counter > 0) {
-            return counter--;
-        }
-
-        return counter = BACKGROUND_ARR.length - 1;
-    });
-})
+//         return counter = BACKGROUND_ARR.length - 1;
+//     });
+// })
 
 
 
